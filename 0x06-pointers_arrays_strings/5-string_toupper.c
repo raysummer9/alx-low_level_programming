1ba4a01b2675d25f970c68ab1a43dc1bfe4f3e0f
raysummer9 <chimepromise433@gmail.com>
2023-10-21 02:00:30 +0100

    convert strings from lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - prints strings to uppercase
+ * @str: parameter
+ * Return: return str
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 'a' + 'A';
+		}
+	}
+	return (str);
+}
