aca24a12b8d8de5e20b8d52506d23450ceeb745f
raysummer9 <chimepromise433@gmail.com>
2023-11-10 02:17:15 +0100

    print characters

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints strings
+ * @separator: separates the string
+ * @n: number of strings
+ * Return: return void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+
+	va_list ptr_str;
+
+	va_start(ptr_str, n);
+
+	for (i = 0; i < n; i++)
+	{	
+
+		str = va_arg(ptr_str, char *);
+		if (str == NULL)
+			printf("(nil)");
+
+		else
+			printf("%s", str);
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr_str);
+}
