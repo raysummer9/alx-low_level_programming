97b2a19fb2839b7aa06016904c6384d36e83e1a6
raysummer9 <chimepromise433@gmail.com>
2023-11-21 14:04:59 +0100

    frees a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: head pointer
+ * Return: return void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL || *head == NULL)
+		return;
+	while ((*head)->next != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	free(*head);
+	*head = NULL;
+}
