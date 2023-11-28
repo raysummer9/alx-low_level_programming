f30ac0326cf5f158b5bac4f69dc1c3b4d0c23293
raysummer9 <chimepromise433@gmail.com>
2023-11-28 19:35:03 +0100

    a function that creates a file and write into it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - create and write into a file and copy
+ * content into it
+ * @filename: the filename
+ * @text_content: text to be copied into the file
+ * Return: return 1 if succesful and -1 if failed
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
